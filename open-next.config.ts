903b88d3ec0121468765f08961e262a8dc604963
armyv7v <enderjpinar@gmail.com>
2025-11-11 20:50:55 -0300

    Add open-next config for Cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 // @ts-nocheck
 
+=======
+>>>>>>> be79a28 (Add open-next config for Cloudflare)
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
